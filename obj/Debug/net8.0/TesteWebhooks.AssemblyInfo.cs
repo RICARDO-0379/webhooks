4973fe2844c90fc3ff4b596684c7adfb50d1c89b
Ricardo Leão <ricardo.leao@email.com>
2025-07-25 16:15:42 -0300

    Habilita Swagger para ambiente de produção

--- obj/Debug/net8.0/TesteWebhooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/TesteWebhooks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteWebhooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7bbcd026a7ead7ed34c950b7af84b10737a224")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteWebhooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteWebhooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
