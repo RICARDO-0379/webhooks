c955b11152a9ddf3f79d9e8405f400f778c807fd
Ricardo Leão <Ricardo Leo@pncq.local>
2025-07-25 14:05:30 -0300

    Adicionando arquivos após ajuste do .gitignore

---
+++ Controllers/PixController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteWebhooks.Model;
+
+namespace TesteWebhooks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PixController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ReceberPix([FromBody] PixNotificacao notificacao)
+        {
+            // Aqui você pode adicionar log, salvar em banco, etc.
+            return Ok("Webhook recebido com sucesso!");
+        }
+    }
+}
---
+++ Model/PixNotificacao.cs
@@ -0,0 +1,35 @@
+﻿namespace TesteWebhooks.Model
+{
+    public class PixNotificacao
+    {
+        public List<PixInfo> pix { get; set; }
+    }
+
+    public class PixInfo
+    {
+        public string endToEndId { get; set; }
+        public string txid { get; set; }
+        public string valor { get; set; }
+        public string horario { get; set; }
+        public string infoPagador { get; set; }
+        public string chave { get; set; }
+        public ComponentesValor componentesValor { get; set; }
+        public object devolucoes { get; set; }
+    }
+
+    public class ComponentesValor
+    {
+        public Valor original { get; set; }
+        public ValorJuros juros { get; set; }
+        public ValorMulta multa { get; set; }
+        public ValorAbatimento abatimento { get; set; }
+        public ValorDesconto desconto { get; set; }
+    }
+
+    public class Valor { public string valor { get; set; } }
+    public class ValorJuros { public string valor_juros { get; set; } }
+    public class ValorMulta { public string valor_multa_documento_cobranca_pix { get; set; } }
+    public class ValorAbatimento { public string valor_abatimento_documento_cobranca_pix { get; set; } }
+    public class ValorDesconto { public string valor_desconto_documento_cobranca_pix { get; set; } }
+
+}
